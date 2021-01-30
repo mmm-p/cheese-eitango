a82037c3188dd04dba3434bfc07eadeaf23c73f0
Yanagawa Mayu <mayu.yanagawa@gmail.com>
2021-01-30 20:14:04 +0900

    arrayでの管理

--- cheese eitango/PlayViewController.swift
+++ cheese eitango/PlayViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PlayViewController: UIViewController,UITextViewDelegate {
-   
+    
     var  saveData: UserDefaults = UserDefaults.standard
     var blindText = ""
     var blindText1 = ""
@@ -35,78 +35,79 @@ class PlayViewController: UIViewController,UITextViewDelegate {
         
         for _ in tango{
             arrayString.append("_ ")
-         
+            
         }
         label.text = arrayString.joined(separator: "")
         
-       
-       
         
-       
+        
+        
+        
     }
     
     @IBAction func check (sender: UIButton){
-       
-               
+        
+        
         let tango  = saveData.object(forKey: "tango") as! String
         let tangocount = tango.unicodeScalars.count
         
-       
+        
         if let alphabet = sender.currentTitle{
-        
-        print(alphabet)
-            if tango.uppercased().contains(alphabet){
-                blindText = ""
-                let bunkatuArray = Array(tango.uppercased())
-               print(bunkatuArray)
-                for i in bunkatuArray {
-                   
-                      print(i)
-                        if String(i) == alphabet{
-                            arrayString.append(alphabet + " ")
-                            print(arrayString)
-
-                            
-                        }else{
-                            arrayString.append("_ ")
-                           print(arrayString)
-
-                        }
-                       
-                        
-                       
-                       
-                      
-
-                }
-         label.text = arrayString.joined(separator: "")
-                
-                
-                
-          }else{
            
-            count = count + 1
-            print(count)
-            let numberImage = "cheese" + String(count)
-            cheeseImageView.image = UIImage(named:numberImage)
-            
-          
-            if count == 6{
-                self.performSegue(withIdentifier: "nocheese", sender: self)
+            print(alphabet)
+            if tango.uppercased().contains(alphabet){
+                
+                var bunkatuArray = Array(tango.uppercased())
+                print(bunkatuArray)
+                for i in 0..<tangocount {
+                    
+                    print(i)
+                    
+                    if String(bunkatuArray[i]) == alphabet{
+                        arrayString[i] = alphabet + ""
+                        print(arrayString)
+                        
+                        
+                    }else{
+                        arrayString[i] = "_ "
+                        print(arrayString)
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+                label.text = arrayString.joined(separator: "")
+                
+                
+                
+            }else{
+                
+                count = count + 1
+                print(count)
+                let numberImage = "cheese" + String(count)
+                cheeseImageView.image = UIImage(named:numberImage)
+                
+                
+                if count == 6{
+                    self.performSegue(withIdentifier: "nocheese", sender: self)
+                }
             }
-          }
-          
-        
             
-       
+            
+            
+            
+        }
+        
     }
-       
-    }
     
     
     
     
-
+    
     
     
     
@@ -115,20 +116,20 @@ class PlayViewController: UIViewController,UITextViewDelegate {
     
     @IBAction func wakatta(){
         
-         
+        
         
     }
- 
     
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
